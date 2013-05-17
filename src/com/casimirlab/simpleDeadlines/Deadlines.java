3a3b456ebae913e2baf4275ee8d98ca625f9e03f
Martin Chaine <chaine.martin@gmail.com>
2013-05-17 20:37:27 +0200

    Use of the new DrawerLayout from the support lib

--- src/com/casimirlab/simpleDeadlines/Deadlines.java
+++ src/com/casimirlab/simpleDeadlines/Deadlines.java
@@ -1,6 +1,5 @@
 package com.casimirlab.simpleDeadlines;
 
-import android.app.ActionBar;
 import android.app.AlertDialog;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.CursorLoader;
@@ -10,16 +9,16 @@ import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.casimirlab.simpleDeadlines.data.DeadlineUtils;
@@ -28,8 +27,9 @@ import com.casimirlab.simpleDeadlines.data.GroupAdapter;
 
 public class Deadlines extends FragmentActivity implements LoaderCallbacks<Cursor>
 {
-  private ActionBar _actionBar;
-  private DrawerLayout _drawer;
+  private String[] _TITLES;
+  private DrawerLayout _drawerLayout;
+  private ActionBarDrawerToggle _drawerToggle;
   private ListView _grouplist;
   private GroupAdapter _groupAdapter;
   private ViewPager _pager;
@@ -42,26 +42,21 @@ public class Deadlines extends FragmentActivity implements LoaderCallbacks<Curso
     super.onCreate(savedInstanceState);
     setContentView(R.layout.deadlines);
 
-    _actionBar = getActionBar();
-    _actionBar.setDisplayShowTitleEnabled(false);
-    _actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
-    SpinnerAdapter navAdapter = ArrayAdapter.createFromResource(this,
-								R.array.act_nav_list,
-								android.R.layout.simple_spinner_dropdown_item);
-    ActionBar.OnNavigationListener navListener = new ActionBar.OnNavigationListener()
-    {
-      public boolean onNavigationItemSelected(int position, long itemId)
-      {
-	_pager.setCurrentItem(position);
-	return true;
-      }
-    };
-    _actionBar.setListNavigationCallbacks(navAdapter, navListener);
+    _TITLES = getResources().getStringArray(R.array.act_nav_list);
 
-    _drawer = new DrawerLayout(this, R.layout.drawer);
+    getActionBar().setDisplayHomeAsUpEnabled(true);
+    getActionBar().setHomeButtonEnabled(true);
+    getActionBar().setIcon(R.drawable.ic_app);
+    getActionBar().setTitle(_TITLES[0]);
+
+    _drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+    _drawerToggle = new ActionBarDrawerToggle(this, _drawerLayout,
+					      R.drawable.ic_act_filter,
+					      android.R.string.unknownName, android.R.string.unknownName);
+    
+
     _groupAdapter = new GroupAdapter(this, null);
-    _grouplist = (ListView)_drawer.findViewById(R.id.grouplist);
-    _grouplist.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+    _grouplist = (ListView)findViewById(R.id.grouplist);
     _grouplist.setOnItemClickListener(new AdapterView.OnItemClickListener()
     {
       public void onItemClick(AdapterView<?> parent, View view, int position, long id)
@@ -73,7 +68,7 @@ public class Deadlines extends FragmentActivity implements LoaderCallbacks<Curso
 	_grouplist.setItemChecked(position, !wasSelected);
 	_currentGroupIdx = _grouplist.getCheckedItemPosition();
 	frag.setGroupFilter(wasSelected ? null : label.getText().toString());
-	_drawer.close();
+	_drawerLayout.closeDrawers();
       }
     });
     _grouplist.setAdapter(_groupAdapter);
@@ -86,7 +81,7 @@ public class Deadlines extends FragmentActivity implements LoaderCallbacks<Curso
       @Override
       public void onPageSelected(int position)
       {
-	_actionBar.setSelectedNavigationItem(position);
+	getActionBar().setTitle(_TITLES[position]);
 	getLoaderManager().restartLoader(0, null, Deadlines.this);
       }
     });
@@ -114,10 +109,18 @@ public class Deadlines extends FragmentActivity implements LoaderCallbacks<Curso
     }
   }
 
+  @Override
+  protected void onPostCreate(Bundle savedInstanceState)
+  {
+    super.onPostCreate(savedInstanceState);
+
+    _drawerToggle.syncState();
+  }
+
   public Loader<Cursor> onCreateLoader(int id, Bundle args)
   {
     Uri.Builder builder = DeadlinesContract.Deadlines.CONTENT_URI.buildUpon();
-    if (_actionBar.getSelectedNavigationIndex() == 1)
+    if (getActionBar().getSelectedNavigationIndex() == 1)
       builder.appendPath(DeadlinesContract.Deadlines.FILTER_ARCHIVED);
     builder.appendPath(DeadlinesContract.GROUPS_PATH);
 
@@ -128,9 +131,6 @@ public class Deadlines extends FragmentActivity implements LoaderCallbacks<Curso
   public void onLoadFinished(Loader<Cursor> loader, Cursor c)
   {
     _groupAdapter.swapCursor(c);
-
-    _actionBar.setIcon(_groupAdapter.getCount() > 0 ? R.drawable.ic_act_filter : R.drawable.app_icon);
-    _actionBar.setDisplayHomeAsUpEnabled(_groupAdapter.getCount() > 0);
   }
 
   public void onLoaderReset(Loader<Cursor> loader)
@@ -151,39 +151,30 @@ public class Deadlines extends FragmentActivity implements LoaderCallbacks<Curso
   {
     if (item.getItemId() == android.R.id.home)
     {
-      _drawer.toggle();
+      if (_drawerLayout.isDrawerOpen(_grouplist))
+	_drawerLayout.closeDrawers();
+      else
+	_drawerLayout.openDrawer(_grouplist);
+
       return true;
     }
     if (item.getItemId() == R.id.act_new)
     {
       actionNew(null);
       return true;
-
-
     }
     else if (item.getItemId() == R.id.act_settings)
     {
       startActivity(new Intent(this, Settings.class));
-
       return true;
     }
     return super.onOptionsItemSelected(item);
   }
 
-  @Override
-  public void onBackPressed()
-  {
-    if (!_drawer.isOpened())
-      super.onBackPressed();
-    else
-      _drawer.close();
-  }
-
   public void actionNew(View v)
   {
     Intent i = new Intent(this, DeadlineEditor.class);
-    i.putExtra(EditorDialogFragment.EXTRA_ISNEW,
-	       true);
+    i.putExtra(EditorDialogFragment.EXTRA_ISNEW, true);
     startActivity(i);
   }
 }
