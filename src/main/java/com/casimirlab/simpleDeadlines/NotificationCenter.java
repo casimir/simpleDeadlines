e886abc388319ee4858749dcb48d7c5568820ba1
Martin Chaine <chaine.martin@gmail.com>
2015-01-06 14:12:11 +0100

    Linting

--- src/main/java/com/casimirlab/simpleDeadlines/NotificationCenter.java
+++ src/main/java/com/casimirlab/simpleDeadlines/NotificationCenter.java
@@ -1,13 +1,22 @@
 package com.casimirlab.simpleDeadlines;
 
-import android.app.*;
-import android.content.*;
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.database.ContentObserver;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.widget.RemoteViews;
+
 import com.casimirlab.simpleDeadlines.provider.DeadlinesContract;
 import com.casimirlab.simpleDeadlines.ui.MainActivity;
 
